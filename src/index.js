14a62a7402e3b010b86f473612d5d5a5c4b549bf
Alex Kiborgok <borgokalek14@gmail.com>
2022-04-14 21:52:30 +0300

    search item from an array of movies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    fetch(`http://localhost:3000/movies/${e.target.searchByID.value}`).then(
+      (res) => res.json()
+    ).then(movie => {
+        let title = document.getElementsByTagName("h4")[0]
+        let summary = document.getElementsByTagName("p")[0]
+        title.innerHTML = movie.title
+        summary.innerHTML = movie.summary;
+    })
+  });
+};
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
